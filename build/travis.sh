ad3d2e0406d2335723d9ae0ba19c1aa72c9fe694
vhrychenko <aptah199494@gmail.com>
2018-05-14 18:24:57 +0300

    Add process repo

--- build/travis.sh
+++ build/travis.sh
@@ -13,6 +13,7 @@ git clone https://github.com/spryker/demoshop.git $SHOP_DIR
 cd $SHOP_DIR
 composer self-update && composer --version
 composer config repositories.logger git https://github.com/spryker-middleware/logger.git
+composer config repositories.logger git https://github.com/spryker-middleware/process.git
 composer install --no-interaction
 mkdir -p data/DE/logs
 chmod -R 777 data/
